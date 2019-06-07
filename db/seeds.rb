fdc42f559ab11fac005630bef3818173f78bb3eb
Justin Love <git@JustinLove.name>
2019-06-07 11:32:07 -0500

    begin building database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..15).each do |n|
+  Server.create({
+    :server_name => "server#{n}.onehouronelife.com",
+    :created_at => Date.new(2019, 3, 9),
+  })
+end
+Server.create({
+  :server_name => "bigserver1.onehouronelife.com",
+  :created_at => Time.at(1548458068),
+  :removed_at => Time.at(1548809524),
+})
+Server.create({
+  :server_name => "bigserver2.onehouronelife.com",
+  :created_at => Time.at(1548804597),
+})
+
