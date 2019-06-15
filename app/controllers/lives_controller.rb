4690cce077057ba1b567ff63b05ba093544ed1d1
Justin Love <git@JustinLove.name>
2019-06-15 11:49:38 -0500

    rename lives controller

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,4 +1,4 @@
-class PointsController < ApplicationController
+class LivesController < ApplicationController
   def index
     render :json => PointPresenter.wrap(
       Life.where(:server_id => 17, :birth_time => ((Date.today - 2)..(Date.today))))
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :points, only: [:index]
+  resources :lives, only: [:index]
   resources :family_trees, only: [:index]
 end
