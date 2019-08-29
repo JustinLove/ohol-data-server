075abe134353bd8599a0aa484b84334c327eac80
Justin Love <git@JustinLove.name>
2019-08-28 19:46:11 -0500

    more options for family tree lookup

--- app/controllers/family_trees_controller.rb
+++ app/controllers/family_trees_controller.rb
@@ -2,23 +2,25 @@ require 'ohol-family-trees/graph'
 
 class FamilyTreesController < ApplicationController
   def index
-    lineage = DB[:lives]
-      .where(:server_id => params[:server_id], :epoch => params[:epoch], :playerid => params[:playerid])
-      .limit(1).pluck(:lineage).first
-    family = DB[:lives]
-      .where(:server_id => params[:server_id], :epoch => params[:epoch], :lineage => lineage)
-      .order(:birth_time)
-    killers = DB[:lives]
-      .where(:server_id => params[:server_id], :epoch => params[:epoch], :playerid => family.where(Sequel.~(:killer => nil)).select(:killer))
-      .order(:birth_time)
+    search = LineageSearch.new(params)
     #render :html => GraphPresenter.html(family).html_safe
 
-    render :plain => GraphPresenter.response(family, killers, [params[:playerid]])
+    render :plain => GraphPresenter.response(search.family, search.killers, [search.playerid].compact)
   end
 
   private
 
   def life_params
-    params.permit(:server_id, :epoch, :playerid)
+    params.permit(
+      :server_id,
+      :epoch,
+      :playerid,
+      :lineage,
+      :server_name,
+      :server_short,
+      :start_time,
+      :end_time,
+      :birth_time,
+      :death_time )
   end
 end
