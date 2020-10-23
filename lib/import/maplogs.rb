d600cbc57ecccb0a82118187322f8186f860dfd2
Justin Love <git@JustinLove.name>
2020-10-23 11:47:15 -0500

    maintain and use maplog archive

--- lib/import/maplogs.rb
+++ lib/import/maplogs.rb
@@ -11,6 +11,7 @@ require 'ohol-family-trees/filesystem_s3'
 
 module Import
   module Maplogs
+    MaplogArchive = 'publicMapChangeData/'
     def self.load_cache(cache, output_dir)
       filesystem = OHOLFamilyTrees::FilesystemLocal.new(output_dir)
       collection = OHOLFamilyTrees::MaplogCache::Servers.new(cache)
@@ -21,6 +22,18 @@ module Import
       filesystem = OHOLFamilyTrees::FilesystemS3.new(output_bucket)
       collection = OHOLFamilyTrees::MaplogServer::Servers.new
       load_collection(collection, filesystem)
+    end
+
+    def self.timestamp_fixup_cache(cache, output_dir)
+      filesystem = OHOLFamilyTrees::FilesystemLocal.new(output_dir)
+      collection = OHOLFamilyTrees::MaplogCache::Servers.new(cache)
+      timestamp_fixup(collection, filesystem)
+    end
+
+    def self.timestamp_fixup_live(output_bucket)
+      filesystem = OHOLFamilyTrees::FilesystemS3.new(output_bucket)
+      collection = OHOLFamilyTrees::MaplogServer::Servers.new
+      timestamp_fixup(collection, filesystem)
     end
 
     def self.load_collection(collection, filesystem)
@@ -45,9 +58,11 @@ module Import
       placement_path = "pl/#{server_id}"
       maplog_path = "pl/#{server_id}"
 
-      list = OHOLFamilyTrees::MaplogList::Logs.new(filesystem, "#{placement_path}/file_list.json")
-      list.update_from(logs)
-      list.checkpoint
+      list = OHOLFamilyTrees::MaplogList::Logs.new(filesystem, "#{placement_path}/file_list.json", MaplogArchive)
+      updated_files = Set.new
+      list.update_from(logs) do |logfile|
+        updated_files << logfile.path
+      end
 
       final_placements = OHOLFamilyTrees::OutputFinalPlacements.new(placement_path, filesystem, objects)
 
@@ -59,7 +74,67 @@ module Import
 
       context = OHOLFamilyTrees::LogfileContext.process(seeds, list)
 
-      logs.each do |logfile|
+      list.each do |logfile|
+        cache_path = logfile.path
+        Raven.extra_context(:logfile => cache_path)
+        #p cache_path
+
+        next unless logfile.placements?
+
+        if logs.has?(logfile.path)
+          logfile = logs.get(logfile.path)
+        end
+
+        #next unless logfile.path.match('000seed')
+        #next unless logfile.path.match('1151446675seed') # small file
+        #next unless logfile.path.match('1521396640seed') # two arcs in one file
+        #next unless logfile.path.match('588415882seed') # one arc with multiple start times
+        #next unless logfile.path.match('2680185702seed') # multiple files one seed
+        #next unless logfile.path.match('471901928seed') # bad middle start line
+
+        if true
+          if updated_files.member?(logfile.path)
+            p 'updated file', logfile.path
+            filesystem.write(MaplogArchive + logfile.path) do |archive|
+              IO::copy_stream(logfile.open, archive)
+            end
+          end
+        end
+        if true
+          final_placements.process(logfile, context[logfile.path])
+        end
+        if true
+          maplog.process(logfile)
+        end
+      end
+
+      list.checkpoint
+    end
+
+    def self.timestamp_fixup(collection, filesystem)
+      objects = OHOLFamilyTrees::ObjectData.new
+      collection.each do |logs|
+        p logs.server
+        Raven.extra_context(:import_server => logs.server)
+        fixup_server(logs, filesystem, objects)
+      end
+    end
+
+    def self.fixup_server(logs, filesystem, objects)
+      server = logs.server.sub('/', '')
+      server_id = Server.where(:server_name => server).pluck(:id).first
+      raise "server not found" if server_id.nil?
+      placement_path = "pl/#{server_id}"
+      maplog_path = "pl/#{server_id}"
+
+      list = OHOLFamilyTrees::MaplogList::Logs.new(filesystem, "#{placement_path}/file_list.json", MaplogArchive)
+      list.update_from(logs)
+
+      final_placements = OHOLFamilyTrees::OutputFinalPlacements.new(placement_path, filesystem, objects)
+
+      maplog = OHOLFamilyTrees::OutputMaplog.new(maplog_path, filesystem, objects)
+
+      list.each do |logfile|
         cache_path = logfile.path
         Raven.extra_context(:logfile => cache_path)
         #p cache_path
@@ -74,10 +149,10 @@ module Import
         #next unless logfile.path.match('471901928seed') # bad middle start line
 
         if true
-          final_placements.process(logfile, context[logfile.path])
+          final_placements.timestamp_fixup(logfile)
         end
         if true
-          maplog.process(logfile)
+          maplog.timestamp_fixup(logfile)
         end
       end
     end
--- lib/tasks/maplogs.rake
+++ lib/tasks/maplogs.rake
@@ -11,4 +11,17 @@ namespace :maplogs do
     output_bucket = ENV['OUTPUT_BUCKET']
     Import::Maplogs.fetch(output_bucket)
   end
+
+  task :timestamp_fixup_test => :environment do
+    require 'import/maplogs'
+    cache = ENV['OHOL_FILE_CACHE'] + 'map'
+    output_dir = ENV['LOCAL_OUTPUT_DIR']
+    Import::Maplogs.timestamp_fixup_cache(cache, output_dir)
+  end
+
+  task :timestamp_fixup => :environment do
+    require 'import/maplogs'
+    output_bucket = ENV['OUTPUT_BUCKET']
+    Import::Maplogs.timestamp_fixup_live(output_bucket)
+  end
 end
